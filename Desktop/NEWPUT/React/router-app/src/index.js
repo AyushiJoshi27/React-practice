73bd792e404354735101127df33f3526bf322eec
AyushiJoshi27 <ayushi.joshi@newput.com>
2023-03-26 23:21:59 +0530

    Router

---
+++ Desktop/NEWPUT/React/router-app/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Home from "./Components/Home";
+import Blogs from "./Components/Blogs";
+import Contact from "./Components/Contact";
+import NoPage from "./Components/NoPage";
+
+export default function AppOne() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppOne />);
+
+reportWebVitals();
